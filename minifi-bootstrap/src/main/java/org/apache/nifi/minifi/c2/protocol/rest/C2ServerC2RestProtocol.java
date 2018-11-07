befb3c800f2c3f32cda0a84e5100b0f3fddcf246
Aldrin Piri <aldrin@apache.org>
2018-11-07 12:48:04 -0500

    Working changes

---
+++ minifi-bootstrap/src/main/java/org/apache/nifi/minifi/c2/protocol/rest/C2ServerC2RestProtocol.java
@@ -0,0 +1,25 @@
+package org.apache.nifi.minifi.c2.protocol.rest;
+
+import org.apache.nifi.minifi.bootstrap.c2.JerseyMiNiFiC2Client;
+import org.apache.nifi.minifi.bootstrap.c2.MiNiFiC2Client;
+import org.apache.nifi.minifi.c2.C2Payload;
+
+public class C2ServerC2RestProtocol extends RestC2Protocol {
+    private final MiNiFiC2Client c2Client;
+
+    public C2ServerC2RestProtocol(String c2serverAddress, int c2ServerPort) {
+        super(c2serverAddress, c2ServerPort);
+        c2Client = new JerseyMiNiFiC2Client.Builder().config(null).build();
+    }
+
+    @Override
+    protected String getHeartbeatEndpoint() {
+        c2Client.getC2ProtocolClient();
+        return null;
+    }
+
+    @Override
+    public C2Payload transmit(C2Payload payload) {
+        return null;
+    }
+}
